05fed72a78d5d2903b4d4ba5eb9fde45161edf68
Jayesh Parekh <jayesh.parekh@space-bx.co.uk>
2024-09-07 15:55:00 +0100

    Factoring work

--- CalculatorService.Library/CalculatorFunctionFactory.cs
+++ CalculatorService.Library/CalculatorFunctionFactory.cs
@@ -1,22 +1,22 @@
-﻿using CalculatorService.Library.Functions;
+﻿using CalculatorService.Library.CalculatorOperators;
 
 namespace CalculatorService.Library;
 
 public interface ICalculatorFunctionFactory
 {
-    IEnumerable<ICalculatorFunction> CalculatorFunctions { get; }
+    IEnumerable<ICalculatorOperation> CalculatorFunctions { get; }
 
-    ICalculatorFunction GetCalculatorFunction(char mathOperator);
+    ICalculatorOperation GetCalculatorFunction(char mathOperator);
 }
 
 public class CalculatorFunctionFactory : ICalculatorFunctionFactory
 {
-    public IEnumerable<ICalculatorFunction> CalculatorFunctions => new ICalculatorFunction[]
+    public IEnumerable<ICalculatorOperation> CalculatorFunctions => new ICalculatorOperation[]
     {
         new AddFunction(),
         new SubtractFunction()
     };
 
-    public ICalculatorFunction GetCalculatorFunction(char mathOperator) => CalculatorFunctions
+    public ICalculatorOperation GetCalculatorFunction(char mathOperator) => CalculatorFunctions
         .Single(_ => _.FunctionType == mathOperator);
 }
---
+++ CalculatorService.Library/CalculatorOperators/ICalculatorOperation.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace CalculatorService.Library.CalculatorOperators;
+
+public interface ICalculatorOperation
+{
+    string Name { get; }
+
+    string Operator { get; }
+    
+    Expression Apply(Expression left, Expression right);
+}
